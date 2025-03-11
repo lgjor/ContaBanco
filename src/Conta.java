9734cf3d63f871b07b720870a0d450fb37deba7c
Lucas Godoy - aú desviante <lucasgodoyjor@gmail.com>
2025-03-11 07:32:09 -0300

    added the documentation comments

--- src/Conta.java
+++ src/Conta.java
@@ -1,11 +1,24 @@
+/**
+ * A classe Conta representa uma conta bancária com informações como nome, agência,
+ * número da conta e saldo.
+ */
 import java.util.Scanner;
 
 public class Conta {
+    /** O nome do titular da conta. */
     String nome;
+    /** O número da agência da conta. */
     int agencia;
+     /** O número da conta. */
     int numeroDaConta;
+    /** O saldo da conta. */
     double saldo;
 
+    /**
+     * Método que lê os dados da conta do usuário através do console.
+     *
+     * @return A instância da classe Conta com os dados lidos.
+     */
     public Conta LeDadosDaConta() {
         Scanner leNovaConta = new Scanner(System.in);
         System.out.println("Digite seu nome: ");
@@ -20,6 +33,11 @@ public class Conta {
         return this;
     }
 
+    /**
+     * Método que imprime as informações da conta no console.
+     *
+     * @param conta A instância da classe Conta a ser impressa.
+     */
     public void imprimirConta(Conta conta) {
         System.out.println( "Olá "+ conta.nome +
                             ", obrigado por criar uma conta em nosso banco, sua agência é "+ String.valueOf(conta.agencia) +
--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,4 +1,14 @@
+/**
+ * A classe ContaTerminal é a classe principal que inicia o aplicativo de conta bancária.
+ */
 public class ContaTerminal {
+     /**
+     * O método principal que cria uma instância da classe Conta, lê os dados da conta
+     * do usuário e imprime as informações da conta no console.
+     *
+     * @param args Os argumentos da linha de comando (não utilizados neste aplicativo).
+     * @throws Exception Se ocorrer um erro durante a leitura dos dados da conta.
+     */
     public static void main(String[] args) throws Exception {
         Conta conta = new Conta();       
         conta.LeDadosDaConta();
